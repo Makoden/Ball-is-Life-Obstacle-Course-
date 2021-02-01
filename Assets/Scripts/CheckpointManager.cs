756aa70afeef14ef28463fc36bc8c5868ffc975b
Makoden <78382668+Makoden@users.noreply.github.com>
2021-02-01 15:28:41 -0500

    Initial commit

---
+++ Assets/Scripts/CheckpointManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointManager : MonoBehaviour
+{
+    public GameObject ball;
+    private Vector3 lastCheckpoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastCheckpoint = ball.transform.position;
+    }
+
+   
+   public void SetNewCheckpoint (Vector3 newCheckpoint)
+    {
+        lastCheckpoint = ball.transform.position;
+        Debug.Log("New Checkpoint Set at: " + newCheckpoint.ToString());
+    }
+        
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name== "Ball")
+        {
+            other.transform.position = lastCheckpoint;
+            Debug.Log("Reset ball to checkpoint");
+        }
+                }
+}
